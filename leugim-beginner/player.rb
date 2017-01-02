4bc1b615957bb26d4bad55c27357e06b7cbf6477
Miguel <mig.burillo@gmail.com>
2017-01-02 12:56:06 +0100

    beginner - level 6

--- leugim-beginner/player.rb
+++ leugim-beginner/player.rb
@@ -1,22 +1,32 @@
 class Player
-  MIN_HP = 14
+  MIN_HP = 16
+  RUN_HP = 10
 
   def play_turn(warrior)
     @last_health = 20 unless @last_health
+    @direction = :backward unless @direction
 
-    if should_rest?(warrior)
+    if should_run_away?(warrior)
+      warrior.walk!(:backward)
+    elsif should_rest?(warrior)
       warrior.rest!
-    elsif warrior.feel.captive?
-      warrior.rescue!
-    elsif not warrior.feel.empty?
-      warrior.attack!
-    else
-      warrior.walk!
+    elsif warrior.feel(@direction).captive?
+      warrior.rescue!((@direction))
+    elsif warrior.feel(@direction).enemy?
+      warrior.attack!((@direction))
+    elsif warrior.feel(@direction).wall?
+      @direction = :forward
+      warrior.walk!(@direction)
+    elsif warrior.feel(@direction).empty?
+      warrior.walk!(@direction)
     end
 
     @last_health = warrior.health
   end
 
+  def should_run_away?(warrior)
+      return (warrior.health < RUN_HP and taking_damage?(warrior))
+  end
 
   def should_rest?(warrior)
       return (warrior.health < MIN_HP and not taking_damage?(warrior))
