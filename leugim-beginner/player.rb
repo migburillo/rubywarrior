6a2767621b012042a94da58fafe651292ce8b467
Miguel <mig.burillo@gmail.com>
2017-01-02 12:33:32 +0100

    beginner - level 4

--- leugim-beginner/player.rb
+++ leugim-beginner/player.rb
@@ -2,12 +2,26 @@ class Player
   MIN_HP = 12
 
   def play_turn(warrior)
-    if not warrior.feel.empty?
-      warrior.attack!
-    elsif warrior.health < MIN_HP
+    @last_health = 20 unless @last_health
+
+    if should_rest?(warrior)
       warrior.rest!
+    elsif not warrior.feel.empty?
+      warrior.attack!
     else
       warrior.walk!
     end
+
+    @last_health = warrior.health
   end
+
+
+  def should_rest?(warrior)
+      return (warrior.health < MIN_HP and not taking_damage?(warrior))
+  end
+
+  def taking_damage?(warrior)
+      return @last_health > warrior.health
+  end
+
 end
