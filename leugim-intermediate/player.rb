5f96b8fbb62ab71940304e2500d545bd4d03f10b
Miguel <mig.burillo@gmail.com>
2017-01-04 16:54:40 +0100

    intermediate - level 3

--- leugim-intermediate/player.rb
+++ leugim-intermediate/player.rb
@@ -1,13 +1,64 @@
 class Player
-  @direction
+  MIN_HP = 10
+  DIRECTIONS = [:left, :right, :forward, :backward]
+  @last_health
 
   def play_turn(warrior)
-    @direction = warrior.direction_of_stairs
+    @last_health = 20 unless @last_health
 
-    if warrior.feel(@direction).empty?
-      warrior.walk!(@direction)
+    if not taking_damage?(warrior) and should_rest?(warrior)
+      warrior.rest!
+    elsif direction = surrounded?(warrior)
+      warrior.bind!(direction)
+    elsif direction = enemy_around?(warrior)
+      warrior.attack!(direction)
+    elsif direction = captives_around?(warrior)
+      warrior.rescue!(direction)
+    elsif warrior.feel(warrior.direction_of_stairs).enemy?
+      warrior.attack!(warrior.direction_of_stairs)
     else
-      warrior.attack!(@direction)
+      warrior.walk!(warrior.direction_of_stairs)
     end
+    @last_health = warrior.health
   end
+
+  def surrounded?(warrior)
+    enemies_count = 0
+    for direction in DIRECTIONS
+      if warrior.feel(direction).enemy?
+        enemies_count = enemies_count + 1
+      end
+      if enemies_count > 1
+        return direction
+      end
+    end
+    return false
+  end
+
+  def enemy_around?(warrior)
+    for direction in DIRECTIONS
+      if warrior.feel(direction).enemy?
+         return direction
+      end
+    end
+    return false
+  end
+
+  def captives_around?(warrior)
+    for direction in DIRECTIONS
+      if warrior.feel(direction).captive?
+        return direction
+      end
+    end
+    return false
+  end
+
+  def taking_damage?(warrior)
+      return @last_health > warrior.health
+  end
+
+  def should_rest?(warrior)
+      return warrior.health < MIN_HP
+  end
+
 end
